0f00b8056b161c38b68f971c2046d462c3917e60
Dilli Luitel <dilliluitel@gmail.com>
2022-01-10 15:04:05 -0600

    Add program features

--- GamingPlatform/SudokuSolver.cs
+++ GamingPlatform/SudokuSolver.cs
@@ -15,13 +15,15 @@ namespace GamingPlatform
         Sudoku sudoku = new Sudoku();
         Puzzle puzzle = new Puzzle();
 
-        const int size = 9;
-        int[,] grid = new int[size, size];
+        int[,] grid = null; 
+        int[,] puzzleState = null;
 
         public SudokuSolver()
         {
-           grid = puzzle.GetPuzzle(0);
-           InitializeComponent();
+            grid = (int[,])puzzle.GetPuzzle(0).Clone();
+            puzzleState = (int[,])puzzle.GetPuzzle(0).Clone();
+
+            InitializeComponent();
         }
 
         //Table Layout for sudoku board
@@ -35,12 +37,19 @@ namespace GamingPlatform
                 for (int j = 0; j < column; j++)
                 {
                     Control C = Board.GetControlFromPosition(j, i);
+
                     C.TextChanged += new System.EventHandler(textBox_TextChanged);
-                    
+                    C.KeyPress += new KeyPressEventHandler(textBox_KeyPress);
+
                     if (grid[i, j] == 0)
+                    {
                         C.Text = "";
+                    }
                     else
+                    {
                         C.Text = $"{grid[i, j]}";
+                        C.ForeColor = Color.Red;
+                    }
                 }
             }
         }
@@ -50,13 +59,27 @@ namespace GamingPlatform
         {
             TextBox tBox = sender as TextBox;
             TableLayoutPanelCellPosition cell = Board.GetPositionFromControl(tBox);
-            if (string.IsNullOrEmpty(tBox.Text))
-                return;
 
+            if (string.IsNullOrEmpty(tBox.Text))
+            {
+                grid[cell.Row, cell.Column] = 0;
+                return;
+            }
             if (int.TryParse(tBox.Text, out int result))
             {
+               if (result < 1 || result > 9)
+                    MessageBox.Show("Invalid Entry");
+
                 grid[cell.Row, cell.Column] = result;
                 tBox.ForeColor = Color.Green;
+            }
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
             }
         }
 
@@ -94,7 +117,7 @@ namespace GamingPlatform
                     }
                     else if (int.TryParse(C.Text, out int result))
                     {
-                            temp = result;
+                        temp = result;
                     }
 
                     if (sudoku.IsValidEntry(grid, temp, i, j))
@@ -121,7 +144,7 @@ namespace GamingPlatform
             {
                 for (int j = 0; j < column; j++)
                 {
-                    grid[i, j] = 0;             //clears the initial grid
+                    grid[i, j] = puzzleState[i,j];            
 
                     Control C = Board.GetControlFromPosition(j, i);
                     tBox = (TextBox)C;
@@ -130,27 +153,34 @@ namespace GamingPlatform
             }
 
             levelComboBox_SelectedIndexChanged(sender, e);
-
         }
 
         private void levelComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            Random random = new Random();
+            int key = random.Next(3);
+
+            int puzzleLevel = 0;
             switch (levelComboBox.SelectedItem)
-                {
-                    case "Easy":
-                        grid = puzzle.GetPuzzle(0);
-                        break;
+            {
+                case "Easy":
+                puzzleLevel = (int) PuzzleLevel.Easy + key; 
+                    break;
 
-                    case "Medium":
-                        grid = puzzle.GetPuzzle(1);
-                        break;
+                case "Medium":
+                puzzleLevel = (int) PuzzleLevel.Medium + key;
+                    break;
 
-                    case "Hard":
-                        grid = puzzle.GetPuzzle(2);
-                        break;
+                case "Hard":
+                puzzleLevel = (int) PuzzleLevel.Hard + key;
+                    break;
 
-                }
-                for (int i = 0; i < Board.Controls.Count; i++)
+            }
+
+            grid = (int[,])puzzle.GetPuzzle(puzzleLevel).Clone();
+            puzzleState = (int[,])puzzle.GetPuzzle(puzzleLevel).Clone();
+
+            for (int i = 0; i < Board.Controls.Count; i++)
                 {
                     Control C = Board.Controls[i];
                     C.TextChanged += new System.EventHandler(textBox_TextChanged);
